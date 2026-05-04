a69174d729fccb73fa34fbc767d252c6b71e8373
Pabli <pabli@example.com>
2026-05-04 10:22:10 -0300

    Migrate to CMake and fix zygisk header URL

--- module/jni/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := mtk_spoof
-LOCAL_SRC_FILES := main.cpp vulkan_hook.cpp dobby/dobby.cpp
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/dobby $(LOCAL_PATH)
-LOCAL_LDLIBS := -llog -ldl
-LOCAL_CPPFLAGS := -std=c++17 -Wall -Wextra
-include $(BUILD_SHARED_LIBRARY)
